bc4a5d0c82cac085b0c6ab0f1acb92aef8e15af7
Jean_Paul <jeanpft@infinitarium.net>
2020-05-26 12:09:23 -0400

    convert PlayerMoveIdleState to a class

--- src/player/PlayerMoveIdleState.js
+++ src/player/PlayerMoveIdleState.js
@@ -1,16 +1,14 @@
-PlayerMoveIdleState.prototype = new BaseState()
-
-function PlayerMoveIdleState()
-{
-
-}
-
-PlayerMoveIdleState.prototype.update = function(player) {  
-    player.vx = player.nextVx * player.walkSpeed
-    player.vy = player.nextVy * player.walkSpeed
-    player.direction = player.nextDirection  
-    player.nextVx = 0
-    player.nextVy = 0
-    player.isMoving = player.vx != 0 || player.vy != 0               
-    player.currentAnimation = Util.parseDirection(player.direction) + " - " + (player.isMoving == true ? "Walking" : "Idle")
+class PlayerMoveIdleState extends BaseState{
+    constructor() {
+        super()
+    }
+    update = function(player) {
+        player.vx = player.nextVx * player.walkSpeed
+        player.vy = player.nextVy * player.walkSpeed
+        player.direction = player.nextDirection
+        player.nextVx = 0
+        player.nextVy = 0
+        player.isMoving = player.vx != 0 || player.vy != 0
+        player.currentAnimation = Util.parseDirection(player.direction) + " - " + (player.isMoving == true ? "Walking" : "Idle")
+    }
 }
